5c3cd151c07f9425fdb1899d8caba55856ebb564
Rami El Beaino <37887173+RamiElBeaino@users.noreply.github.com>
2022-11-03 11:28:00 -0400

    Lounes -> Initialized MainAppController

--- src/main/java/edu/vanier/collisions/controllers/MainAppController.java
+++ src/main/java/edu/vanier/collisions/controllers/MainAppController.java
@@ -1,1 +1,38 @@
-// je suis beau
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.template.controllers;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import static javafx.scene.paint.Color.color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author 2137827
+ */
+public class MainAppController {
+    
+ @FXML
+ Rectangle cart1;
+ @FXML
+ Rectangle cart2;
+ 
+ 
+ 
+}
