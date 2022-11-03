cc001b4e681448cb961b751e9be93fbab5daa03b
Rami El Beaino <37887173+RamiElBeaino@users.noreply.github.com>
2022-11-03 11:11:16 -0400

    Update MainApp
    Created FXML loader and updated MainApp

--- src/main/java/edu/vanier/collisions/views/MainApp.java
+++ src/main/java/edu/vanier/collisions/views/MainApp.java
@@ -1,29 +1,34 @@
-package edu.vanier.collisions.views;
+package edu.vanier.template;
 
+import edu.vanier.template.controllers.MainAppController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * 
+ * @link: https://openjfx.io/javadoc/18/
+ * @see: Build Scripts/build.gradle
+ * @author Bozos
+ */
 public class MainApp extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // TODO: fxml
-//        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/UIDesignFinal1.fxml"));
+        loader.setController(new MainAppController());
+        Pane root = loader.load();
+        Scene scene = new Scene(root, 850, 710);
+        stage.setScene(scene);
+        stage.setTitle("This is a JavaFX app template...");
+        stage.sizeToScene();
+        stage.show();
+    }
 
-        VBox root = new VBox();
-        // stage is the top level container
-        primaryStage.setTitle("Collisions");
-        primaryStage.show();
-
-        // scene is the container for the content
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Collisions");
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
