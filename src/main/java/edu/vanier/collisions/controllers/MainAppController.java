7fd6e975d7d7a352a446e910ba5e13aef19fcebe
Lounes Azzoun <2126895@edu.vaniercollege.qc.ca>
2022-11-18 15:16:43 -0500

    Added ID for FXML objects

--- src/main/java/edu/vanier/collisions/controllers/MainAppController.java
+++ src/main/java/edu/vanier/collisions/controllers/MainAppController.java
@@ -2,7 +2,7 @@ package edu.vanier.collisions.controllers;
 
 import edu.vanier.collisions.models.Ball;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -10,14 +10,23 @@ public class MainAppController {
     @FXML
     Rectangle car1, car2;
     @FXML
-    Button stopBtn;
+    Button playBtn, stopBtn, resetBtn, importBtn, exportBtn;
+    @FXML
+    TextField car1VelocityTextField, car1MassTextField, car2VelocityTextField, car2MassTextField;
+    @FXML
+    Slider playbackSlider, physicSlider;
+    @FXML
+    MenuButton terrainType;
+    @FXML
+    Menu fileMenuBarbtn, editMenuBarbtn, helpMenuBarbtn;
+
     PhysicsEngine physicsEngine = PhysicsEngine.getInstance();
 
     @FXML
     private void initialize() {
         System.out.println("MainAppController.initialize()...");
-        Ball ball1 = new Ball(Color.PURPLE, 20, 100, -2, 20);
-        Ball ball2 = new Ball(Color.BLUE, 20, 200, 40, 20);
+        Ball ball1 = new Ball(Color.RED, 20, 100, -2, 20);
+        Ball ball2 = new Ball(Color.GREEN, 20, 200, 40, 20);
         car1.setFill(ball1.getColor());
         car2.setFill(ball2.getColor());
         physicsEngine.addEntity(ball1);
@@ -26,7 +35,7 @@ public class MainAppController {
         physicsEngine.setCoefficientOfRestitution(0.8);
 
         // TODO: simulation loop (should it be managed by the physics engine?)
-        stopBtn.setOnAction(e -> {
+        playBtn.setOnAction(e -> {
             physicsEngine.next();
             car1.setX(ball1.getPosX());
             car2.setX(ball2.getPosX());
